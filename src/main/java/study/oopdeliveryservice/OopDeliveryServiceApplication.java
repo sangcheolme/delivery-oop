feb630929eaa9e25b17b8a0cdb65d0b87a4fa30e
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-08-16 09:37:47 +0900

    init: 조영호 우아한 객체지향 세미나 v1

---
+++ src/main/java/study/oopdeliveryservice/OopDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package study.oopdeliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopDeliveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopDeliveryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/oopdeliveryservice/OopDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package study.oopdeliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopDeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
